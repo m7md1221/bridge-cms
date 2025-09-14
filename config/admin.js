6b29ce9762eb361abb094ead5cebc0d6ce9f67ee
m7md1221 <derghamdawa@gmail.com>
2025-09-14 13:18:52 +0300

    ready for deploy16

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'changeme'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
