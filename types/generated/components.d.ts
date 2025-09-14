8e7cfe1d6ebd2250c9896264e026b73e091d6555
m7md1221 <derghamdawa@gmail.com>
2025-09-14 07:45:42 +0300

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,21 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface StepsContent extends Struct.ComponentSchema {
+  collectionName: 'components_steps_contents';
+  info: {
+    displayName: 'Content';
+  };
+  attributes: {
+    Imp_note: Schema.Attribute.String;
+    text: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'steps.content': StepsContent;
+    }
+  }
+}
