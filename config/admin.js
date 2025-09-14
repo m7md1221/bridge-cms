4068d75065dc03366bc4dae6b3767f5485cf6fb5
m7md1221 <derghamdawa@gmail.com>
2025-09-14 12:54:52 +0300

    ready for deploy14

--- config/admin.js
+++ config/admin.js
@@ -1,11 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    // خلي قيمة ثابتة مباشرة بدل env
-    secret: 'VAwSIe67uWtjfYeghKqKwg==',
+    secret: env('ADMIN_JWT_SECRET', 'mySuperSecretKey'),
   },
-  apiToken: {
-    // خلي قيمة ثابتة مباشرة بدل env
-    salt: 'ufveMyB/bMDcafJcZ8t1sA==',
-  },
-  watchIgnoreFiles: ['**/local.js'],
 });
