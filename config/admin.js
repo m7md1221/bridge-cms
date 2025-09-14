4a79f594186174e3928343adcf2f1514484adc9e
m7md1221 <derghamdawa@gmail.com>
2025-09-14 10:14:29 +0300

    ready for deploy9

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,11 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('AUTH_SECRET', 'VAwSIe67uWtjfYeghKqKwg=='), // fallback
+    secret: env('AUTH_SECRET') || crypto.randomBytes(16).toString('base64'),
   },
   apiToken: {
-    salt: env('ADMIN_JWT_SALT', 'AQobwizR1Hw5uZS9A9oDww=='), // fallback
+    salt: env('ADMIN_JWT_SALT') || crypto.randomBytes(16).toString('base64'),
   },
+  watchIgnoreFiles: ['**/local.js'], 
 });
