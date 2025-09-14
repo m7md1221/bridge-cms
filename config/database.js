490dd5f6b264c6d8a2bd48cf68ee9644612e2010
m7md1221 <derghamdawa@gmail.com>
2025-09-14 21:46:03 +0300

    ready for deploy16(database2)

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'your-db-host'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'your-db-name'),
-      user: env('DATABASE_USERNAME', 'your-db-user'),
-      password: env('DATABASE_PASSWORD', 'your-db-password'),
-      ssl: {
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false) && {
         rejectUnauthorized: false,
       },
     },
