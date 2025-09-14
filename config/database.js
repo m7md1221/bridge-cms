13fcc0681e8d413b1a1fc94426dc273a93e181ed
m7md1221 <derghamdawa@gmail.com>
2025-09-14 23:25:39 +0300

    Fix self-signed certificate for Render2

--- config/database.js
+++ config/database.js
@@ -7,9 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'bridge_steps'),
       user: env('DATABASE_USERNAME', 'bridge_steps_user'),
       password: env('DATABASE_PASSWORD', 'Xt8p3dsP91xzqQ339fMEVpcvlDExpYPH'),
-      ssl: {
-        rejectUnauthorized: false, // لازم عشان self-signed certificate
-      },
+      ssl: 0,
     },
   },
 });
