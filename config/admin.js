a8e12a1aa1abfb05328d4e2eb07399ff21f7de26
m7md1221 <derghamdawa@gmail.com>
2025-09-14 10:31:56 +0300

    ready for deploy10

--- config/admin.js
+++ config/admin.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     secret: env('AUTH_SECRET') || crypto.randomBytes(16).toString('base64'),
   },
   apiToken: {
-    salt: env('ADMIN_JWT_SALT') || crypto.randomBytes(16).toString('base64'),
+    salt: env('ADMIN_JWT_SALT', 'AQobwizR1Hw5uZS9A9oDww=='), 
   },
   watchIgnoreFiles: ['**/local.js'], 
 });
