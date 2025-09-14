ffdc336bec0f82f4354603101aaf43ccf30ba161
m7md1221 <derghamdawa@gmail.com>
2025-09-14 10:59:50 +0300

    ready for deploy12

--- config/admin.js
+++ config/admin.js
@@ -2,10 +2,11 @@ const crypto = require('crypto');
 
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('AUTH_SECRET') || crypto.randomBytes(16).toString('base64'),
+    secret: env('AUTH_SECRET', 'VAwSIe67uWtjfYeghKqKwg=='),
   },
   apiToken: {
-    salt: env('ADMIN_JWT_SALT') || crypto.randomBytes(16).toString('base64'),
+    // خلي قيمة ثابتة مباشرة، بدل الاعتماد على env فقط
+    salt: env('ADMIN_JWT_SALT', 'ufveMyB/bMDcafJcZ8t1sA=='),
   },
   watchIgnoreFiles: ['**/local.js'],
 });
