53258e02c72e5c7e7aa138aa952bf78978525bd3
m7md1221 <derghamdawa@gmail.com>
2025-09-14 21:59:56 +0300

    ready for deploy17

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_HOST', 'dpg-d33gkm8dl3ps738sfpl0-a'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false) && {
+      database: env('DATABASE_NAME', 'bridge_steps'),
+      user: env('DATABASE_USERNAME', 'bridge_steps_user'),
+      password: env('DATABASE_PASSWORD', 'Xt8p3dsP91xzqQ339fMEVpcvlDExpYPH'),
+      ssl: {
         rejectUnauthorized: false,
       },
     },
